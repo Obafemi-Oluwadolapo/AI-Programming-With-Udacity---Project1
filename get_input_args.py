c7397239d97a0710fbe845382ec19648a1761458
Obafemi-Oluwadolapo <69119155+Obafemi-Oluwadolapo@users.noreply.github.com>
2022-09-01 07:44:35 +0100

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: OBAEMI OLUWADOLAPO SUCCESS
 # DATE CREATED: 27TH AUGUST, 2022          
-# REVISED DATE: 
+# REVISED DATE: 27TH AUGUST, 2022  
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
