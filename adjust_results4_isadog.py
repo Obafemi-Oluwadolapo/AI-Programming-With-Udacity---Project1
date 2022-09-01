c052d5b7afdb57c3a26deb1bd97f949427e4d98b
Obafemi-Oluwadolapo <69119155+Obafemi-Oluwadolapo@users.noreply.github.com>
2022-09-01 08:02:04 +0100

    Update adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -8,6 +8,7 @@
 
 def adjust_results4_isadog(results_dic, dogfile):
     #dogfile = dognames.txt
+    # OPening an empty dictionary
     dognames_dic = dict()
 
     # Reads in dognames from file, 1 name per line & automatically closes file
@@ -15,8 +16,7 @@ def adjust_results4_isadog(results_dic, dogfile):
         # Reads in dognames from first line in file
         line = infile.readline()
 
-        # Processes each line in file until reaching EOF (end-of-file) by 
-        # processing line and adding dognames to dognames_dic with while loop
+        # Iterates each line in file until reaching the end by iterating line and adding dognames to dognames_dic with while loop
         while line != "":
 
             # Process line by striping newline from line
@@ -24,14 +24,12 @@ def adjust_results4_isadog(results_dic, dogfile):
             
             line = line.replace('\n','')
             
-            # adds dogname(line) to dogsnames_dic if it doesn't already exist 
-            # in the dogsnames_dic dictionary
+            # adds dogname(line) to dogsnames_dic if it doesn't already exist in the dogsnames_dic dictionary
             if line not in dognames_dic:
                 dognames_dic[line] = 1
                 #dognames_dic[line].replace('\n', '')
 
-            # Reads in next line in file to be processed with while loop
-            # if this line isn't empty (EOF)
+            # Reads in next line in file to be processed with while loop if this line isn't empty
             line = infile.readline()
     #print(dognames_dic)       
     for key in results_dic:
@@ -41,10 +39,8 @@ def adjust_results4_isadog(results_dic, dogfile):
                 if results_dic[key][0] in dognames_dic:
                     
                     # Comparing Classifier label with the dog names in the dognames_dic
-                    # This is for check for the similar names in pet name label in alignment with the image of dog 
-                    # in Classifier label found in Dog names Dictionary
-                    # Classifier Label IS image of Dog (e.g. found in dognames_dic)
-                    # appends (1, 1) because both labels are dogs
+                    # This is for check for the similar names in pet name label in alignment with the image of dog in Classifier label found in Dog names Dictionary
+                    # Classifier Label IS image of Dog (e.g. found in dognames_dic) appends (1, 1) because both labels are dogs
                     if results_dic[key][1] in dognames_dic:
                         results_dic[key].extend((1, 1))
 
@@ -56,13 +52,11 @@ def adjust_results4_isadog(results_dic, dogfile):
                         # Pet Image Label IS NOT a Dog image (e.g. NOT found in dognames_dic)
                 else:
 
-                        # Classifier Label IS image of Dog (e.g. found in dognames_dic)
-                        # appends (0, 1)because only Classifier label is a dog
+                        # Classifier Label IS image of Dog (e.g. found in dognames_dic) appends (0, 1)because only Classifier label is a dog
                         if results_dic[key][1] in dognames_dic:
                             results_dic[key].extend((0, 1))
 
-                            # Classifier Label IS NOT image of Dog (e.g. NOT in dognames_dic)
-                            # appends (0, 0) because both labels aren't dogs
+                            # Classifier Label IS NOT image of Dog (e.g. NOT in dognames_dic) appends (0, 0) because both labels aren't dogs
                         else:
                             results_dic[key].extend((0, 0))
     print(results_dic)
